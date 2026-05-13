70ddbbca65a8bc26428b2665db078a03715b8709
lendali <gacavain@gmail.com>
2026-05-12 20:09:10 -0600

    Suma de dos numeros

--- main.py
+++ main.py
@@ -4,3 +4,18 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+    #SUMA DE DOS NUMEROS
+
+    public class Main {
+        public static void main (String[]args) {
+            double a = 10;
+            double b = 20;
+            double resultado = sumar (a,b);
+            System.outprintln ("El resultado es:" + resultado);
+            }
+
+            public static double sumar(double n1, double n2) {
+                return n1 +n2;
+                }
+}

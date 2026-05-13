95f1227d177be49273dbfff400899854db7b7215
lendali <gacavain@gmail.com>
2026-05-13 10:46:34 -0600

    Restando 2 numeros

--- main.py
+++ main.py
@@ -2,20 +2,23 @@ def main():
     print('Hello, World!')
     print("Hola de nuevo, Mundo!")
 
+resultado = suma()
+    print(f"La suma de los dos numeros es: {resultado}")
+def suma():
+    num1= int(input("Ingrese el primer numero: "))
+        num2= int(input("Ingrese el segundo numero: "))
+
+resultado = resta()
+        print(f"La resta de los dos numeros es: {resultado}")
+ def resta():
+    num1 = int (input("Ingrese el primer numero: "))
+    num2 = int (input("Ingrese el segundo numero: "))
+
+        return num1 - num2
+
 if __name__ == "__main__":
     main()
 
-    #SUMA DE DOS NUMEROS
 
-    public class Main {
-        public static void main (String[]args) {
-            double a = 10;
-            double b = 20;
-            double resultado = sumar (a,b);
-            System.outprintln ("El resultado es:" + resultado);
-            }
+    
 
-            public static double sumar(double n1, double n2) {
-                return n1 +n2;
-                }
-}
